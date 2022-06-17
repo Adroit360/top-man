f56b5ed3794024cad43241015328f373fc3d1b93
Moro-Afriyie <ceremoinc99@gmail.com>
2022-06-17 17:43:01 +0000

    feat:create the scroll to top button and add functionality

--- topman/src/app/core/core.module.ts
+++ topman/src/app/core/core.module.ts
@@ -7,9 +7,10 @@ import { CheckoutComponent } from './checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CheckoutItemComponent } from './checkout-item/checkout-item.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 
 @NgModule({
-  declarations: [HomeComponent, CheckoutComponent, CheckoutItemComponent, NavbarComponent],
+  declarations: [HomeComponent, CheckoutComponent, CheckoutItemComponent, NavbarComponent, ScrollToTopComponent],
   imports: [CommonModule, CoreRoutingModule, ReactiveFormsModule],
 })
 export class CoreModule {}
