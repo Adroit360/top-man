de0650353a6ee59dfea4d01529e68d4f07d8e33f
Moro-Afriyie <ceremoinc99@gmail.com>
2022-06-17 12:52:13 +0000

    feat:create the checkout item component

--- topman/src/app/core/core.module.ts
+++ topman/src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { CoreRoutingModule } from './core-routing.module';
 import { HomeComponent } from './home/home.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CheckoutItemComponent } from './checkout-item/checkout-item.component';
 
 @NgModule({
-  declarations: [HomeComponent, CheckoutComponent],
+  declarations: [HomeComponent, CheckoutComponent, CheckoutItemComponent],
   imports: [CommonModule, CoreRoutingModule, ReactiveFormsModule],
 })
 export class CoreModule {}
