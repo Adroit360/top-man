7ec4e79dba0b4ef3846ada85f3ba4c591a432408
Moro-Afriyie <ceremoinc99@gmail.com>
2022-06-17 11:15:50 +0000

    feat:create the checkout page

--- topman/src/app/core/checkout/checkout.component.ts
+++ topman/src/app/core/checkout/checkout.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { cities } from '../models/locations';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
+  checkoutForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\+233\d{9}|^233\d{9}|^\d{10}$/),
+    ]),
+    location: new FormControl('', [Validators.required]),
+  });
+  submitted = false;
+  locations: { name: string; price: number }[] = cities;
+  invalidLocation = false;
+  deliveryFee: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.checkoutForm.controls;
   }
 
+  onSubmit() {
+    this.submitted = true;
+    if (this.checkoutForm.invalid) {
+      window.scroll(0, 0);
+      return;
+    }
+
+    if (this.invalidLocation) {
+      // this.isValidLocation = true;
+      return;
+    }
+  }
+
+  onCalculateDeliveryFee(event: any): void {
+    const selectedLocation = event.target.value;
+    const city: { name: string; price: number } | undefined =
+      this.locations.find((item) => item.name === selectedLocation);
+    if (!city) {
+      this.invalidLocation = true;
+      this.deliveryFee = 0;
+    } else {
+      this.invalidLocation = false;
+      // this.orderForm.patchValue({
+      //   deliveryFee: city.price.toFixed(2),
+      // });
+      this.deliveryFee = city.price;
+      //this.totalPrice = this.getTotalPrice(this.deliveryFee, this.priceOfFood);
+    }
+  }
 }
--- topman/src/app/core/core.module.ts
+++ topman/src/app/core/core.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { HomeComponent } from './home/home.component';
 import { CheckoutComponent } from './checkout/checkout.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    CheckoutComponent
-  ],
-  imports: [
-    CommonModule,
-    CoreRoutingModule
-  ]
+  declarations: [HomeComponent, CheckoutComponent],
+  imports: [CommonModule, CoreRoutingModule, ReactiveFormsModule],
 })
-export class CoreModule { }
+export class CoreModule {}
