3f83f7f1f94386c8a36255f1cf559dea7c475fa8
Moro-Afriyie <ceremoinc99@gmail.com>
2022-06-17 17:27:42 +0000

    feat:create the navbar

--- topman/src/app/core/checkout-item/checkout-item.component.ts
+++ topman/src/app/core/checkout-item/checkout-item.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-checkout-item',
   templateUrl: './checkout-item.component.html',
-  styleUrls: ['./checkout-item.component.scss']
+  styleUrls: ['./checkout-item.component.scss'],
 })
 export class CheckoutItemComponent implements OnInit {
+  //TODO: pass props from parent to this component
+  //TODO: incrase and decrease quantity fucntionality
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
