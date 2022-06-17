39720e03437e3f147cb1bc3b4e3fc663106c6b99
Moro-Afriyie <ceremoinc99@gmail.com>
2022-06-17 10:38:38 +0000

    feat:create the books

--- topman/src/app/core/home/home.component.ts
+++ topman/src/app/core/home/home.component.ts
@@ -9,6 +9,20 @@ export class HomeComponent implements OnInit {
   dummyData = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
+  categories = [
+    'aki ola',
+    'fiction',
+    'aki ola',
+    'a+ series',
+    'aki ola',
+    'aki ola',
+    'Science & Fiction',
+    'GNAT',
+    'Boat Books',
+    'Apo Galore',
+    'Aki ola',
+    'the books company',
+  ];
   constructor() {}
 
   ngOnInit(): void {}
